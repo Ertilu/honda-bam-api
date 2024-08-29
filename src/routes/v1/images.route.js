a7474b13a16c5ac20793823cf1f126d6d61a00c1
Reza Raka Nugraha <rezaraka00@gmail.com>
2024-08-29 23:14:58 +0700

    enable multer

--- src/routes/v1/images.route.js
+++ src/routes/v1/images.route.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const imagesController = require('../../controllers/images.controller');
-// const multer = require('multer');
+const multer = require('multer');
 const crypto = require('crypto');
 const path = require('path');
 
-// const storage = multer.diskStorage({
-//   destination: 'uploads',
-//   filename: function (req, file, cb) {
-//     crypto.randomBytes(20, (err, buf) => {
-//       cb(null, buf.toString('hex') + path.extname(file.originalname));
-//     });
-//   },
-// });
+const storage = multer.diskStorage({
+  destination: 'uploads',
+  filename: function (req, file, cb) {
+    crypto.randomBytes(20, (err, buf) => {
+      cb(null, buf.toString('hex') + path.extname(file.originalname));
+    });
+  },
+});
 
-// const upload = multer({ dest: '/uploads' });
+const upload = multer({ storage });
 
 const router = express.Router();
 
