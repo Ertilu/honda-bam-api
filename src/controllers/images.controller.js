4c918592f259d80f413916b32c038ef468ee6524
Reza Raka Nugraha <rezaraka00@gmail.com>
2024-09-13 23:49:58 +0700

    fix image upload5

--- src/controllers/images.controller.js
+++ src/controllers/images.controller.js
@@ -25,17 +25,21 @@ const upload = catchAsync(async (req, res) => {
     //   res.send(JSON.parse(response.body));
     // });
 
-    // var obj = {
-    //   name: req.file.originalname,
-    //   desc: req.file.mimetype,
-    //   image: {
-    //     data: fs.readFileSync(req.file.path),
-    //     contentType: 'image/png',
-    //   },
-    // };
-    // const result = await imagesService.createImage(obj);
+    var obj = {
+      name: req.file.originalname,
+      desc: req.file.mimetype,
+      image: {
+        data: fs.readFileSync(req.file.path),
+        contentType: 'image/png',
+      },
+    };
+    const result = await imagesService.createImage(obj);
 
-    res.send('obj');
+    res.send(
+      res.send({
+        image: { url: `data:${result.image.contentType};base64, ${Buffer.from(result.image.data).toString('base64')}` },
+      })
+    );
   } catch (err) {
     res.send(err);
   }
