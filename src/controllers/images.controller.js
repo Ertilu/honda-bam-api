dfcf329d37b2a802906d5f8bc8ad9fd1f8e97b35
Reza Raka Nugraha <rezaraka00@gmail.com>
2024-09-13 23:43:28 +0700

    fix image upload3

--- src/controllers/images.controller.js
+++ src/controllers/images.controller.js
@@ -5,36 +5,40 @@ const path = require('path');
 const { imagesService } = require('../services');
 
 const upload = catchAsync(async (req, res) => {
-  if (!fs.existsSync('/tmp')) {
-    fs.mkdirSync('/tmp');
+  try {
+    if (!fs.existsSync('/tmp')) {
+      fs.mkdirSync('/tmp');
+    }
+
+    // const source = fs.createReadStream(req.file.path);
+
+    // const options = {
+    //   method: 'POST',
+    //   url: 'https://freeimage.host/api/1/upload?key=6d207e02198a847aa98d0a2a901485a5&format=json',
+    //   headers: {},
+    //   formData: { source },
+    // };
+
+    // request(options, function (error, response) {
+    //   fs.unlinkSync(req.file.path);
+    //   if (error) throw new Error(error);
+    //   res.send(JSON.parse(response.body));
+    // });
+
+    var obj = {
+      name: req.file.originalname,
+      desc: req.file.mimetype,
+      image: {
+        data: fs.readFileSync(req.file.path),
+        contentType: 'image/png',
+      },
+    };
+    // const result = await imagesService.createImage(obj);
+
+    res.send(obj);
+  } catch (err) {
+    res.send(err);
   }
-
-  // const source = fs.createReadStream(req.file.path);
-
-  // const options = {
-  //   method: 'POST',
-  //   url: 'https://freeimage.host/api/1/upload?key=6d207e02198a847aa98d0a2a901485a5&format=json',
-  //   headers: {},
-  //   formData: { source },
-  // };
-
-  // request(options, function (error, response) {
-  //   fs.unlinkSync(req.file.path);
-  //   if (error) throw new Error(error);
-  //   res.send(JSON.parse(response.body));
-  // });
-
-  var obj = {
-    name: req.file.originalname,
-    desc: req.file.mimetype,
-    image: {
-      data: fs.readFileSync(req.file.path),
-      contentType: 'image/png',
-    },
-  };
-  // const result = await imagesService.createImage(obj);
-
-  res.send(obj);
 });
 
 module.exports = {
