c23b97559c85a6e391ecfd94514c7b9cf6365911
Reza Raka Nugraha <rezaraka00@gmail.com>
2024-09-16 21:11:12 +0700

    upload image using cloudinary

--- src/controllers/images.controller.js
+++ src/controllers/images.controller.js
@@ -3,6 +3,62 @@ const request = require('request');
 const fs = require('fs');
 const path = require('path');
 const { imagesService } = require('../services');
+const { v2 } = require('cloudinary');
+
+const freeImageHosting = (req, res) => {
+  /** free image hosting */
+  // const source = fs.createReadStream(req.file.path);
+  // const options = {
+  //   method: 'POST',
+  //   url: 'https://freeimage.host/api/1/upload?key=6d207e02198a847aa98d0a2a901485a5&format=json',
+  //   headers: {},
+  //   formData: { source },
+  // };
+  // request(options, function (error, response) {
+  //   fs.unlinkSync(req.file.path);
+  //   if (error) throw new Error(error);
+  //   res.send(JSON.parse(response.body));
+  // });
+};
+
+const uploadToMongoDb = (req, res) => {
+  /** upload to mongodb */
+  // var obj = {
+  //   name: req.file.originalname,
+  //   desc: req.file.mimetype,
+  //   image: {
+  //     data: fs.readFileSync(req.file.path),
+  //     contentType: 'image/png',
+  //   },
+  // };
+  // const result = await imagesService.createImage(obj);
+  // res.send({
+  //   image: { url: `data:${result.image.contentType};base64, ${Buffer.from(result.image.data).toString('base64')}` },
+  // });
+};
+
+const cloudinary = async (req, res) => {
+  const cloudinary = v2;
+  // Configuration
+  cloudinary.config({
+    cloud_name: 'dthdnryp3',
+    api_key: '616178389367325',
+    api_secret: '3ZaTxgGioe7ynwPfM4jP2ijT_PI', // Click 'View API Keys' above to copy your API secret
+  });
+
+  // Upload an image
+
+  // const source = fs.createReadStream(req.file.path);
+  const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+    folder: 'catalogues',
+    public_id: req.file.filename,
+    resource_type: 'auto',
+  });
+
+  res.send({
+    image: { url: uploadResult?.secure_url },
+  });
+};
 
 const upload = catchAsync(async (req, res) => {
   try {
@@ -10,34 +66,8 @@ const upload = catchAsync(async (req, res) => {
       fs.mkdirSync('/tmp');
     }
 
-    // const source = fs.createReadStream(req.file.path);
-
-    // const options = {
-    //   method: 'POST',
-    //   url: 'https://freeimage.host/api/1/upload?key=6d207e02198a847aa98d0a2a901485a5&format=json',
-    //   headers: {},
-    //   formData: { source },
-    // };
-
-    // request(options, function (error, response) {
-    //   fs.unlinkSync(req.file.path);
-    //   if (error) throw new Error(error);
-    //   res.send(JSON.parse(response.body));
-    // });
-
-    var obj = {
-      name: req.file.originalname,
-      desc: req.file.mimetype,
-      image: {
-        data: fs.readFileSync(req.file.path),
-        contentType: 'image/png',
-      },
-    };
-    const result = await imagesService.createImage(obj);
-
-    res.send({
-      image: { url: `data:${result.image.contentType};base64, ${Buffer.from(result.image.data).toString('base64')}` },
-    });
+    /** cloudinary */
+    cloudinary(req, res);
   } catch (err) {
     res.send(err);
   }
